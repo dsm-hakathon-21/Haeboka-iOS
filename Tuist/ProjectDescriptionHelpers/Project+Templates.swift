57738e6632ffad376ddb8dd290d51c8f55a707c2
박준하 <goodjunha@gmail.com>
2024-07-16 19:10:43 +0900

    (#16) 린트 삭제

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -35,7 +35,7 @@ public extension Project {
             infoPlist: infoPlist,
             sources: sources,
             resources: resources,
-            scripts: [.SwiftLintString],
+//            scripts: [.SwiftLintString],
             dependencies: dependencies
         )
         
