ce4d09f30e7347dbc2a8162fa0017db804cd7acb
Choyoungjun <cyj070513@gmail.com>
2024-07-17 10:31:24 +0900

    (#15) :: 단어장

--- Projects/Haeboka-iOS/Sources/Application/SceneDelegate.swift
+++ Projects/Haeboka-iOS/Sources/Application/SceneDelegate.swift
@@ -14,7 +14,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     private func start() {
-        window?.rootViewController = BaseNC(rootViewController: TranslateViewController())
+        window?.rootViewController = BaseNC(rootViewController: WordSetViewController())
         window?.makeKeyAndVisible()
     }
 
